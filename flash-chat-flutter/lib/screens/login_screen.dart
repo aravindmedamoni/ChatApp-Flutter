c13a8ec0d04e23b533a7af6b55fbe7628fd7b495
aravindmedamoni <aravnd.accurate@gmail.com>
2019-09-24 23:51:13 +0530

    added model spinner to the screen for showing progress

--- flash-chat-flutter/lib/screens/login_screen.dart
+++ flash-chat-flutter/lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'chat_screen.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = '/loginScreen';
@@ -13,6 +14,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
   String userEmail;
   String userPassword;
 
@@ -20,80 +22,87 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Hero(
-                  tag: 'logo',
-                  child: Container(
-                    height: 200.0,
-                    child: Image.asset('images/logo.png'),
-                  ),
-                ),
-                SizedBox(
-                  height: 48.0,
-                ),
-                TextField(
-                  style: TextStyle(
-                    color: Colors.black87,
-                  ),
-                  onChanged: (value) {
-                    //Do something with the user input.
-                    userEmail = value;
-                  },
-                  decoration: kTextFieldDecoration.copyWith(
-                    labelText: 'Enter your Email',
-                    prefixIcon: Icon(
-                      Icons.mail,
-                      color: Colors.grey,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Hero(
+                    tag: 'logo',
+                    child: Container(
+                      height: 200.0,
+                      child: Image.asset('images/logo.png'),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                TextField(
-                  obscureText: true,
-                  style: TextStyle(
-                    color: Colors.black87,
+                  SizedBox(
+                    height: 48.0,
                   ),
-                  onChanged: (value) {
-                    //Do something with the user input.
-                    userPassword = value;
-                  },
-                  decoration: kTextFieldDecoration.copyWith(
-                    labelText: 'Enter your password',
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.grey,
+                  TextField(
+                    style: TextStyle(
+                      color: Colors.black87,
+                    ),
+                    onChanged: (value) {
+                      //Do something with the user input.
+                      userEmail = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                      labelText: 'Enter your Email',
+                      prefixIcon: Icon(
+                        Icons.mail,
+                        color: Colors.grey,
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 24.0,
-                ),
-                Padding(
-                    padding: EdgeInsets.symmetric(vertical: 30.0),
-                    child: RoundedButton(
-                        colour: Colors.lightBlueAccent,
-                        onPress: () async {
-                          try{
-                            final user = await _auth.signInWithEmailAndPassword(email: userEmail, password: userPassword);
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    style: TextStyle(
+                      color: Colors.black87,
+                    ),
+                    onChanged: (value) {
+                      //Do something with the user input.
+                      userPassword = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                      labelText: 'Enter your password',
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24.0,
+                  ),
+                  Padding(
+                      padding: EdgeInsets.symmetric(vertical: 30.0),
+                      child: RoundedButton(
+                          colour: Colors.lightBlueAccent,
+                          onPress: () async {
+                            setState(() {
+                              showSpinner = true;
+                            });
+                            try{
+                              final user = await _auth.signInWithEmailAndPassword(email: userEmail, password: userPassword);
 
-                            if(user != null){
-                              Navigator.pushNamed(context, ChatScreen.id);
+                              if(user != null){
+                                Navigator.pushNamed(context, ChatScreen.id);
+                              }
+                              showSpinner = false;
+                            }catch(e){
+                              print(e);
                             }
-                          }catch(e){
-                            print(e);
-                          }
-                        },
-                        buttonName: 'Log In')),
-              ],
+                          },
+                          buttonName: 'Log In')),
+                ],
+              ),
             ),
           ),
         ),
--- flash-chat-flutter/lib/screens/registration_screen.dart
+++ flash-chat-flutter/lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'chat_screen.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = '/registerScreen';
@@ -14,88 +15,98 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
 
   final _auth = FirebaseAuth.instance;
+  bool shwSpinner = false;
   String userMail;
   String userPassword;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0,vertical: 20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Hero(
-                tag: 'logo',
-                child: Container(
-                  height: 200.0,
-                  child: Image.asset('images/logo.png'),
-                ),
-              ),
-              SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                style: TextStyle(
-                  color: Colors.black87,
-                ),
-                onChanged: (value) {
-                  //Do something with the user input.
-                  userMail = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                  labelText: 'Enter Your Email',
-                  prefixIcon: Icon(
-                    Icons.mail,
-                    color: Colors.grey,
+      body: ModalProgressHUD(
+        inAsyncCall: shwSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0,vertical: 20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                obscureText: true,
-                keyboardType: TextInputType.visiblePassword,
-                style: TextStyle(
-                  color: Colors.black87,
+                SizedBox(
+                  height: 48.0,
                 ),
-                onChanged: (value) {
-                  //Do something with the user input.
-                  userPassword = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                  labelText: 'Enter your password',
-                  prefixIcon: Icon(
-                    Icons.lock,
-                    color: Colors.grey,
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(
+                    color: Colors.black87,
+                  ),
+                  onChanged: (value) {
+                    //Do something with the user input.
+                    userMail = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                    labelText: 'Enter Your Email',
+                    prefixIcon: Icon(
+                      Icons.mail,
+                      color: Colors.grey,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 24.0,
-              ),
-              Padding(
-                  padding: EdgeInsets.symmetric(vertical: 30.0),
-                  child: RoundedButton(
-                      colour: Colors.blueAccent,
-                      onPress: () async {
-                        try{
-                          final newUser = await _auth.createUserWithEmailAndPassword(email: userMail, password: userPassword);
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  style: TextStyle(
+                    color: Colors.black87,
+                  ),
+                  onChanged: (value) {
+                    //Do something with the user input.
+                    userPassword = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                    labelText: 'Enter your password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                Padding(
+                    padding: EdgeInsets.symmetric(vertical: 30.0),
+                    child: RoundedButton(
+                        colour: Colors.blueAccent,
+                        onPress: () async {
+                          setState(() {
+                            shwSpinner = true;
+                          });
+                          try{
+                            final newUser = await _auth.createUserWithEmailAndPassword(email: userMail, password: userPassword);
 
-                          if(newUser != null){
-                            Navigator.pushNamed(context, ChatScreen.id);
+                            if(newUser != null){
+                              Navigator.pushNamed(context, ChatScreen.id);
+                            }
+                            setState(() {
+                              shwSpinner = false;
+                            });
+                          }catch(e){
+                            print(e);
                           }
-                        }catch(e){
-                          print(e);
-                        }
 
-                      },
-                      buttonName: 'Register'))
-            ],
+                        },
+                        buttonName: 'Register'))
+              ],
+            ),
           ),
         ),
       ),
